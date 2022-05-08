32c6ad87586e1493c83788c896958310daf5a9fd
Gabriel G <base@gbr.pw>
2022-05-07 23:51:06 -0500

    Search for more TE models excluding bundles, parts and cases. Total results up to 100.

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from rich.table import Table
 import os
 
 #webpage to scrape
-scrapeTarget = 'https://www.thomann.de/intl/search_dir.html?sw=TE-62CC&smcs=0bd321_3144'
+scrapeTarget = 'https://www.thomann.de/intl/search_dir.html?sw=Harley%20Benton%20TE-62&smcs=0bd321_3144'
+scrapeTarget = 'https://www.thomann.de/intl/search_dir.html?smcs=0bd321_2481&oa=prd&sw=Harley%20Benton%20TE%20-bundle%20-case%20-parts%20-lh&filter=true&ls=100'
 shipment = 63
 currency = "COP"
+rows = 0
 
 #actual html of page
 page = urllib.request.urlopen(scrapeTarget)
@@ -29,7 +31,8 @@ today = date.today().strftime("%d/%m/%Y")
 for singleItem in soup.find_all('div', attrs={'class': 'product'}):
     title = singleItem.find(class_="title__manufacturer").text + " " + singleItem.find(class_="title__name").text.strip()
     color = ""
-    
+    rows = rows+1
+
     if not 'Bundle' in title:
         if 'LPB' in title:
             color = "Lake Placid Blue"
@@ -43,20 +46,25 @@ for singleItem in soup.find_all('div', attrs={'class': 'product'}):
             color = "Dakota Red"
         elif 'SFG' in title:
             color = "Sea Foam Green"
+        elif 'BK' in title:
+            color = "Black High Gloss"
+        elif 'CAR' in title:
+            color = "Candy Apple Red"
 
         availability = singleItem.find(class_="product__availability").text.strip()
-        price = singleItem.find(class_="product__price-primary").text.replace("$", "").strip()
-        price = int(price) + shipment
+        price = singleItem.find(class_="product__price-primary").text.replace("$", "").replace(",", ".").strip()
+        price = float(price) + shipment
 
         if currency == "COP":
             price = os.popen("forx -q " + str(price) + " usd cop").read().replace("$", "").replace(",", "").strip()
-            price = round(float(price))
-            
+        
+        price = round(float(price))
         price = "$" + str(price) + " " + currency
         table.add_row(today, title, color, availability, price)
 
 console = Console()
 console.print(table)
+console.print(str(rows) + " results")
 console.print(scrapeTarget + "\n")
 
 """ 
