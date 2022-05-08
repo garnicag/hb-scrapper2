14a88fd213af67f2322b0475d3e9fb56ee72a853
Gabriel G <base@gbr.pw>
2022-05-07 21:55:28 -0500

    Results output improved using rich tables, fixes in currency display

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import urllib
 from datetime import date
 from bs4 import BeautifulSoup
-from colorama import Fore, Back, Style
+from rich.console import Console
+from rich.table import Table
 
 #webpage to scrape
 scrapeTarget = 'https://www.thomann.de/intl/search_dir.html?sw=TE-62CC&smcs=0bd321_3144'
@@ -12,11 +13,20 @@ page = urllib.request.urlopen(scrapeTarget)
 #page parsed for bs4
 soup = BeautifulSoup(page, 'html.parser')
 
+table = Table(title="\n Harley Benton Availability from Thomann")
+
+table.add_column("Date", style="red")
+table.add_column("Model", style="green", no_wrap=False)
+table.add_column("Color", style="yellow")
+table.add_column("Availability", style="blue")
+table.add_column("Price", style="magenta")
+
+today = date.today().strftime("%d/%m/%Y")
+
 for singleItem in soup.find_all('div', attrs={'class': 'product'}):
-    today = Fore.RED + date.today().strftime("%d/%m/%Y")
-    title = Fore.GREEN + singleItem.find(class_="title__manufacturer").text + " " + singleItem.find(class_="title__name").text.strip()
-    dash = Fore.WHITE + "-"
+    title = singleItem.find(class_="title__manufacturer").text + " " + singleItem.find(class_="title__name").text.strip()
     color = ""
+    
     if not 'Bundle' in title:
         if 'LPB' in title:
             color = "Lake Placid Blue"
@@ -31,11 +41,15 @@ for singleItem in soup.find_all('div', attrs={'class': 'product'}):
         elif 'SFG' in title:
             color = "Sea Foam Green"
 
-        availability = Fore.BLUE + singleItem.find(class_="product__availability").text.strip()
-        price = Fore.MAGENTA + singleItem.find(class_="product__price-primary").text.strip()
-        print(today, dash, title + " (" + color + ")", dash, availability, dash, price)
+        availability = singleItem.find(class_="product__availability").text.strip()
+        price = "$" + singleItem.find(class_="product__price-primary").text.replace("$", "").strip() + " USD"
+        table.add_row(today, title, color, availability, price)
+
+console = Console()
+console.print(table)
+console.print(scrapeTarget + "\n")
 
 """ 
 with open('index.txt', 'w') as output_file:
     output_file.write(result)
- """
+"""
