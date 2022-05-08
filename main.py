23a3dc7ce262a5ab555ff7cb0e5aa682054f4314
Gabriel G <base@gbr.pw>
2022-05-07 22:27:19 -0500

    Price now includes shipment. Added price conversion to COP

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from datetime import date
 from bs4 import BeautifulSoup
 from rich.console import Console
 from rich.table import Table
+import os
 
 #webpage to scrape
 scrapeTarget = 'https://www.thomann.de/intl/search_dir.html?sw=TE-62CC&smcs=0bd321_3144'
+shipment = 63
+currency = "COP"
 
 #actual html of page
 page = urllib.request.urlopen(scrapeTarget)
@@ -13,13 +16,13 @@ page = urllib.request.urlopen(scrapeTarget)
 #page parsed for bs4
 soup = BeautifulSoup(page, 'html.parser')
 
-table = Table(title="\n Harley Benton Availability from Thomann")
+table = Table(title="\n Harley Benton availability from Thomann")
 
 table.add_column("Date", style="red")
 table.add_column("Model", style="green", no_wrap=False)
 table.add_column("Color", style="yellow")
 table.add_column("Availability", style="blue")
-table.add_column("Price", style="magenta")
+table.add_column("Price + Sh", style="magenta")
 
 today = date.today().strftime("%d/%m/%Y")
 
@@ -42,7 +45,14 @@ for singleItem in soup.find_all('div', attrs={'class': 'product'}):
             color = "Sea Foam Green"
 
         availability = singleItem.find(class_="product__availability").text.strip()
-        price = "$" + singleItem.find(class_="product__price-primary").text.replace("$", "").strip() + " USD"
+        price = singleItem.find(class_="product__price-primary").text.replace("$", "").strip()
+        price = int(price) + shipment
+
+        if currency == "COP":
+            price = os.popen("forx -q " + str(price) + " usd cop").read().replace("$", "").replace(",", "").strip()
+            price = round(float(price))
+            
+        price = "$" + str(price) + " " + currency
         table.add_row(today, title, color, availability, price)
 
 console = Console()
